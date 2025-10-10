53ea1e73d4192d7e9862083988783706a4ee26fa
jmmckechnie <jmmckechnie@gmail.com>
2025-10-10 19:16:48 +0100

    Initial commit

---
+++ l3_digital_support_technician.py
@@ -0,0 +1,123 @@
+criteria = {
+"1. Digital Technologies K1 S1 B2 B4":
+"""
+K1: Digital office automation technologies;
+ • how to use them to create, update, edit, manage, and present data• the organisation’s use of templates and their best working practice
+ • how these tools can be used to collaborate with others
+S1: Use digital technologies, including collaborative tools, to operate effectively as part of a team, and with other stakeholders, enabling sharing of information and best practice.
+B2: Use own initiative when implementing digital technologies and finding solutions to stakeholder’s problems.
+B4: Self-motivated for example: takes responsibility to complete the job.
+
+Pass Criteria
+Describes the use of a range of digital technologies to share information and best practice with co-workers, team members and external stakeholders (S1)
+Explains their approach to using digital office automation technologies using their initiative to get the job done (K1 B2 B4)
+
+Distinction Criteria
+Evaluates the use of digital office automation technologies to the organisation (K1, B2, B4)
+""",
+
+"2. Digital and Information Security K3 K4 K8 S3":
+"""
+K3: The importance of backing up data securely and the technologies that support it.
+K4: The principles, processes and procedures for the secure handling of data in compliance with legislation.
+K8: Approaches to risk mitigation for data loss including confidentiality, integrity and availability
+S3: Apply information security principles, for example: information transfer, deletion, storage, usage and communications that may include using mobile devices.
+
+Pass Criteria
+Explains the importance of backing up data securely and the technologies that support it (K3)
+Describes how to apply data security processes and procedures and comply with current legislation for the secure handling of data. (K4)
+Summarises the approaches to risk mitigation for data loss including confidentiality, integrity and availability. (K8)
+Explains how they apply information security principles, for example: information transfer; deletion; storage; usage and communications that may include using mobile devices (S3)
+
+Distinction Criteria
+Evaluates the application of security measures to mitigate and protect data integrity, during transfer, storage and sharing (K4, S3)
+""",
+
+"3. Digital Information Management Systems K7 S5":
+"""
+K7: Principles of a helpdesk system, including accessing and maintaining stakeholder information and the contribution of helpdesk system to the organisations performance and customer service.
+S5: Operate digital information systems, for example
+ • Management- Finance Human Resources
+ • Bespoke departmental or organisational systems or databases
+
+Pass Criteria
+Explains what is meant by a helpdesk system, how to use it for accessing and maintaining stakeholder information and the contribution of helpdesk systems to the organisation's performance and customer service. (K7)
+Describes how they operate digital information systems for example:
+ • management, finance or human resources systems
+ • other bespoke departmental or organisational systems or databases (S5)
+
+Distinction Criteria
+Outlines the impact of their use of digital information systems on the organisation’s performance (K7,S5)
+""",
+
+"4. Communication K10 K11 S6":
+"""
+K10: Approaches to a range of communication channels and how to adapt to different audiences and situations.
+K11: The principles and constraints of searching the internet and accessing information securely
+ • Currency- Relevance- Authority- Accuracy- Purpose
+S6: Communicate effectively through a variety of different channels using terminology appropriate to the audience
+
+Pass Criteria
+Describes how they communicate through a variety of different channels using terminology appropriate to the audience (K10, S6)
+Explains the constraints of searching the internet and accessing information securely by reference to
+currency; relevance; authority; accuracy; purpose (CRAAP) (K11)
+
+Distinction Criteria
+N/A
+""",
+
+"5. Digital Learning K12 K15 S7":
+"""
+K12: Approaches to planning and organising own learning activities to maintain and develop digital skills (CPD).
+K15: Current and emerging digital technologies and the possible implications for work on a support desk including the impacts of digital technologies for climate change, sustainability and moving to net carbon zero
+S7: Use digital resources to extend own knowledge and skills relevant to their role
+
+Pass Criteria
+Describes planning and organising their own learning to maintain and develop digital skills relevant to their role’ (S7, K12)
+Summarises current and emerging digital technologies and the possible implications for their work including the impacts of climate change, sustainability and the move to net carbon zero. (K15)
+
+Distinction Criteria
+N/A
+""",
+
+"6. Continuous Improvement K14 S9 B5":
+"""
+K14: Principles of continuous improvement within the context of the application and use of digital technologies and the benefits
+S9: Use digital systems to identify productivity and performance improvements
+B5: Takes a sustainable mindset towards digital support activities ensuring climate change and the move to net carbon zero by 2050 is a consideration
+
+Pass Criteria
+Summarises the need for continuous improvement with the application and use of digital technologies and the benefits (K14)
+Explains how they use digital systems to identify productivity and performance improvements including the consideration of sustainable approaches (S9, B5)  
+
+Distinction Criteria
+N/A
+""",
+
+"7. Teamwork K13 S10":
+"""
+K13: Approaches to effective time management and prioritisation
+S10: Use digital technologies to operate effectively as part of a team, and with other stakeholders, enabling sharing of information and best practice
+
+Pass Criteria
+Outlines the organisational approaches to time management and prioritisation (K13)
+Describes the use of digital technologies to operate effectively within a team and enable the sharing of information and best practice (S10)
+
+Distinction Criteria
+Evaluates the importance and impact of their work on other team members (K13,S10)
+""",
+
+"8. Application Skills Support K19 K21 S14":
+"""
+K19: Working practices for the productive use and administration of stakeholder’s applications.
+K21: Approaches to the training and support of stakeholders to make the best use of the organisation’s applications.
+S14: Coach and guide stakeholders to develop their applications skills to use digital systems effectively.
+
+Pass Criteria
+Explains working practices for productive use and administration of stakeholder’s applications. (K19)
+Describes how they coach and guide stakeholders to develop their applications skills to use digital systems effectively (K21, S14)
+
+Distinction Criteria
+Evaluates the impact of the coaching and guidance provided to stakeholders (K21,S14)
+"""
+}
