fd3fb37322685e16aea8e0c2545a2732d7222999
jmmckechnie <jmmckechnie@gmail.com>
2025-10-15 17:18:39 +0100

    Add L4 software dev criteria

---
+++ l4_software_developer.py
@@ -0,0 +1,113 @@
+criteria = {
+"Theme 1: Lifecycle and Methodology":
+"""
+K1: all stages of the software development life-cycle (what each stage contains, including the inputs and outputs)
+K3: the roles and responsibilities of the project life-cycle within your organisation, and your role
+K5: the similarities and differences between different software development methodologies, such as agile and waterfall.
+
+Pass Criteria:
+Describes all stages of the software development lifecycle (K1)
+Describes the roles and responsibilities of the project lifecycle within their organisation, and their role (K3)
+Describes the similarities and differences between different software development methodologies, such as agile and waterfall (K5)
+
+There are no distinction criteria.
+""",
+
+"Theme 2: Communication":
+"""
+K4: how best to communicate using the different communication methods and how to adapt appropriately to different audiences
+S15: communicate software solutions and ideas to technical and non-technical stakeholders
+B7: Communicates effectively in a variety of situations to both a technical and non-technical audience.
+
+Pass Criteria:
+Describes methods of communicating with all stakeholders that is determined by the audience and/o  their level of technical knowledge. (K4, S15)
+Explains how they have communicated effectively in a variety of situations to both a technical and non-technical audience. (B7)
+
+Distinction Criteria:
+Compares and contrasts the different types of communication used for technical and non-technical audiences and the benefits of these types of communication methods (K4, S15, B7)
+""",
+
+"Theme 3: Reuse":
+"""
+K7: software design approaches and patterns, to identify reusable solutions to commonly occurring problems
+
+Pass Criteria:
+Suggests and applies different software design approaches and patterns, to identify reusable solutions to commonly occurring problems (include Bespoke or off-the-shelf) (K7)
+
+Distinction Criteria:
+Evaluates and recommends approaches to using reusable solutions to common problems. (K7)
+""",
+
+"Theme 4: Testing":
+"""
+K12: software testing frameworks and methodologies
+S5: conduct a range of test types, such as Integration, System, User Acceptance, Non-Functional, Performance and Security testing.
+S13: follow testing frameworks and methodologies
+
+Pass Criteria:
+Describes basic software testing frameworks and methodologies (K12)
+Illustrates how to conduct test types, such as Integration, System, User Acceptance, Non-Functional, Performance and Security testing including how they have followed testing frameworks and methodologies (S5, S13)
+
+Distinction Criteria:
+Evaluates the use of various software testing frameworks and methodologies and justifies their choice. (K12)
+""",
+
+"Theme 4: Data":
+"""
+S3: link code to data sets
+K10: principles and uses of relational and non-relational databases
+
+Pass Criteria:
+Explains, how they have linked code to data sets (S3)
+Applies the principles and uses of relational and non-relational databases to software development tasks (K10)
+
+There are no distinction criteria.
+""",
+
+"Theme 5: Policy":
+"""
+K8: organisational policies and procedures relating to the tasks being undertaken, and when to follow them. For example the storage and treatment of GDPR sensitive data.
+S14: follow company, team or client approaches to continuous integration, version and source control
+B5: Acts with integrity with respect to ethical, legal and regulatory ensuring the protection of personal data, safety and security.
+
+Pass Criteria:
+Explains the relevance of organisational policies and procedures relating to the tasks being undertaken, and when to follow them including how they have followed company, team or client approaches to continuous integration, version, and source control (K8 S14)
+Explains how they have established an approach in the workplace which reflects integrity with respect to ethical, legal, and regulatory matters and ensures the protection of personal data, safety and security. (B5)
+
+There are no distinction criteria.
+""",
+
+"Theme 6: Analysis and Design":
+"""
+S2: develop effective user interfaces
+S8: create simple software designs to effectively communicate understanding of the program
+S9: create analysis artefacts, such as use cases and/or user stories
+S17: interpret and implement a given design whist remaining compliant with security and maintainability requirements
+
+Pass Criteria:
+Explains, their own approach to development of user interfaces (S2)
+Creates simple software designs to communicate understanding of the programme to stakeholders and users of the programme (S8)
+Creates analysis artefacts, such as use cases and/or user stories to enable effective delivery of software activities (S9)
+Explains, how they have interpreted and implemented a given design whilst remaining compliant with security and maintainability requirements (S17)
+
+There are no distinction criteria.
+""",
+
+"Theme 7: Working independently and as part of a team":
+"""
+B1: Works independently and takes responsibility. For example, has a disciplined and responsible approach to risk and stays motivated and committed when facing challenges
+B4: Works collaboratively with a wide range of people in different roles, internally and externally, with a positive attitude to inclusion & diversity
+B6: Shows initiative and takes responsibility for solving problems within their own remit, being resourceful when faced with a problem to solve.
+B8: Shows curiosity to the business context in which the solution will be used, displaying an inquisitive approach to solving the problem. This includes the curiosity to explore new opportunities, techniques and the tenacity to improve methods and maximise performance of the solution and creativity in their approach to solutions.
+B9: Committed to continued professional development.
+
+Pass Criteria:
+Describes, how they have operated independently to complete tasks to given deadlines which reflect the level of responsibility assigned to them by the organisation’. (B1)
+Illustrates how they have worked collaboratively with people in different roles, internally and externally, which show a positive attitude to inclusion & diversity. (B4)
+Illustrates their approach to meeting unexpected minor changes at work and outlines their approach to delivering within their remit using their initiative. (B6)
+Illustrates how they have responded to the business context with curiosity to explore new opportunities and techniques with tenacity to improve solution performance, establishing an approach to methods and solutions which reflects a determination to succeed (B8)
+Explains how they reflect on their continued professional development and act independently to seek out new opportunities (B9)
+
+There are no distinction criteria.
+"""
+}
